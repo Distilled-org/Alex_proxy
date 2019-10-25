0c1ff3d189785e2ed3f7bd8ec71408277bbf3449
alexcravalho <a.d.cravalho@gmail.com>
2019-10-24 17:29:54 -0700

    deployment ready proxy

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Alex_proxy
+
+To begin:
+Navigate to the root directory of the project
+
+1. npm install
--- server.js
+++ server.js
@@ -4,9 +4,72 @@ const app = express();
 const PORT = 4001 || process.env.PORT;
 const bp = require('body-parser');
 const path = require('path');
+const axios = require('axios');
 
 app.use(express.static('public'));
 app.use(bp.json());
+
+const selamContainer = 'http://ec2-18-191-231-115.us-east-2.compute.amazonaws.com:7000';
+const alexContainer = 'http://ec2-34-217-10-22.us-west-2.compute.amazonaws.com:4000';
+
+app.get('/app.bundle.js', (req, res) => {
+  //get request to hosted EC2 alex app
+  axios.get(`${alexContainer}/app.bundle.js`)
+    .then((response) => {
+      res.send(response.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send(500)
+    })
+});
+//get request to hosted EC2 Selam app (for her app bundle)
+app.get('/app2.bundle.js', (req, res) => {
+  axios.get(`${selamContainer}/app2.bundle.js`)
+    .then((response) => {
+      res.send(response.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send(500)
+    })
+})
+// get request to hosted EC2 Selam app (for vendor bundle)
+app.get('/vendor.bundle.js', (req, res) => {
+  axios.get(`${selamContainer}/vendor.bundle.js`)
+  .then((response) => {
+    res.send(response.data)
+  })
+  .catch((err) => {
+    console.log(err)
+    res.send(500)
+  })
+})
+
+//get request for items to selam's app
+app.get('/item/:id', (req, res) => {
+  const id = req.params.id;
+  axios.get(`${selamContainer}/item/${id}`)
+    .then((response) => {
+      res.send(response.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send(500)
+    })
+})
+//get request for items to alex's app
+app.get('/items/:id', (req, res) => {
+  const id = req.params.id;
+  axios.get(`${alexContainer}/items/${id}`)
+    .then((response) => {
+      res.send(response.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send(500)
+    })
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
